c133503f7055905deee3643b577792c1c28436f7
shansou504 <123512155+shansou504@users.noreply.github.com>
2023-07-23 20:45:08 -0400

    Delete src/usr/local/bin directory
    Added streaming command into nginx rtmp block so it no longer needs to be started by auto login and can be run by www-data user.

--- src/usr/local/bin/stream.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-pkill -f libcamera
-libcamera-vid --nopreview -t 0 --inline --codec libav --libav-format flv --libav-audio --audio-device "alsa_input.usb-C-Media_Electronics_Inc._USB_Ear-Microphone-00.mono-fallback" -o "rtmp://localhost:1935/hls/camera"
